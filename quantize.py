9c1008df71994cde275a059ba999681c09669b79
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2022-07-26 16:44:20 -0400

    made quantization faster

--- quantize.py
+++ quantize.py
@@ -34,12 +34,9 @@ def quantize(num_bits: int, weights: torch.Tensor, full_precision: torch.Tensor)
         item = list([x.item() for x in all_binary_values[i]])
         item.append(all_possible_values[i].item())
         combo.append(item)
-    combo.sort(key=lambda x: x[-1])
     combo = torch.Tensor(combo)
     all_binary_values = combo[:, :-1]
     all_possible_values = torch.Tensor(combo[:, -1])
-
-    size = list(full_precision.size())[0]
     flat_precision = torch.clone(full_precision.flatten())
     min_mse = torch.ones_like(flat_precision) * 10000
     indicies = torch.zeros_like(flat_precision)
@@ -49,19 +46,42 @@ def quantize(num_bits: int, weights: torch.Tensor, full_precision: torch.Tensor)
         new_mse = torch.minimum(mse,min_mse)
         mse_delta = min_mse - new_mse
         min_mse = new_mse
-        new_mask = torch.where(mse_delta==0,0,1)
-        orig_mask = torch.where(mse_delta==0,1,0)
-        new_indicies = torch.ones_like(flat_precision) * i * new_mask + indicies * orig_mask
-        indicies = new_indicies
+        indicies = torch.where(mse_delta!=0,i,indicies)
     quantized_value = all_possible_values[indicies.long()]
     quantized_binary = all_binary_values[indicies.long()]
     return quantized_value, quantized_binary
 
+def old_quantize(num_bits: int, weights: torch.Tensor, full_precision: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    all_binary_values = torch.Tensor(
+        list(itertools.product([-1, 1], repeat=num_bits)))
+    all_possible_values = torch.matmul(
+        all_binary_values.to(torch.float), weights.to(torch.float))
+    combo = []
+    for i in range(len(all_binary_values)):
+        item = list([x.item() for x in all_binary_values[i]])
+        item.append(all_possible_values[i].item())
+        combo.append(item)
+    combo.sort(key=lambda x: x[-1])
+    combo = torch.Tensor(combo)
+    all_binary_values = combo[:, :-1]
+    all_possible_values = combo[:, -1]
+
+    quantized_value = torch.zeros_like(full_precision)
+    size = list(full_precision.size())[0]
+    quantized_binary = torch.zeros((size, num_bits))
+    for i, x in enumerate(full_precision.flatten()):
+        idx = torch.argmin(torch.abs(all_possible_values-x.item()))
+        quantized_value[i] = all_possible_values[idx]
+        quantized_binary[i] = all_binary_values[idx]
+    return quantized_value, quantized_binary
 
 if __name__ == "__main__":
+    import timeit
     powers_of_2 = torch.arange(3)
     powers_of_2 = 2**powers_of_2
-    arr = torch.randint(-10, 10, [8])
+    arr = torch.randint(-10, 10, [8000])
     print(arr)
     print(quantize(3, powers_of_2, arr))
+    #  print(timeit.timeit('old_quantize(3,powers_of_2,arr)',number=100,globals={'old_quantize':old_quantize,'powers_of_2':powers_of_2,'arr':arr}))
+    print(timeit.timeit('quantize(3,powers_of_2,arr)',number=10000,globals={'quantize':quantize,'powers_of_2':powers_of_2,'arr':arr}))
 # def quantize_grad()
