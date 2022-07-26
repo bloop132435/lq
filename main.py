67325047ed901b4b816d1dd46a7d0bab61966156
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2022-07-26 19:58:36 -0400

    update lqnet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from torchvision import transforms
 import os
 import torch
+import math
 import torchvision
 import torch.nn as nn
 from quantize import quantize
@@ -9,10 +10,10 @@ device = 'cpu'
 if torch.cuda.is_available():
     device = 'cuda'
     print('cuda')
-model = torchvision.models.resnet18()
+model_q = torchvision.models.resnet18()
 
 batch_size = 100
-bit_precision = 8
+bit_precision = 3
 
 
 mnist_folder = os.path.join(os.path.dirname(__file__), 'data')
@@ -35,7 +36,7 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
 
 criterion = nn.CrossEntropyLoss()
 
-quantized = [p for n,p in model.named_parameters() if 'bias' not in n and 'bn' not in n and 'downsample' not in n]
+quantized = [p for n,p in model_q.named_parameters() if 'bias' not in n and 'bn' not in n and 'downsample' not in n]
 optimizer_quantized = torch.optim.Adam(quantized, lr=0.001)
 
 full_precision = quantized.copy()
@@ -77,49 +78,53 @@ constants = [
         25600,
         25600,
         ]
-weights = [torch.Tensor(powers_of_2.to(torch.float))/constants[i//3]
-           for i,(name,_) in enumerate(model.named_parameters()) if 'bias' not in name and 'bn' not in name and 'downsample' not in name]
-
+weights = []
+index = 0
+for i,(name,_) in enumerate(model_q.named_parameters()):
+    if 'bias' not in name and 'bn' not in name and 'downsample' not in name:
+        weights.append( torch.Tensor(powers_of_2) / constants[index])
+        index+=1
 num_epochs = 5
 
-
-print("a")
+binary_representation = [torch.Tensor([])
+        for _ in range(len(quantized))]
+PERIOD = 1
 def train():
     total_step = len(train_loader)
     for epoch in range(num_epochs):
         for index, (images, labels) in enumerate(train_loader):
-            print("b")
             images = images.to(device)
             labels = labels.to(device)
             # TODO, convert full precision to quantized
             # list of 8-long bool list describing each layer
-            binary_representation = [torch.Tensor([])
-                                     for _ in range(len(quantized))]
-            print("c")
             for i, f in enumerate(full_precision):
-                flat_quantized, binary_representation[i] = quantize(
-                    bit_precision, weights[i], f.flatten())
-                quantized[i].data = flat_quantized.reshape(quantized[i].size())
-                # binr = torch.where(
-                #     binary_representation[i] > 0, 1, 0).to(torch.float)
-                binr = (torch.clone(binary_representation[i]))
-                b = torch.t(binr)
-                bbt = torch.matmul(b,torch.t(b))
-                inv = torch.inverse(bbt)
-                bbtb = torch.matmul(inv,b)
-                intermediate = torch.matmul(bbtb,f.flatten())
-                weights[i] = intermediate
-                print(i,end='')
+                l = len(str(i))
+                print(i,end='',flush=True)
+                for _ in range(PERIOD):
+                    flat_quantized, binary_representation[i] = quantize(
+                        bit_precision, weights[i], f)
+                    quantized[i].data = flat_quantized.reshape(quantized[i].size())
+                    # binr = torch.where(
+                    #     binary_representation[i] > 0, 1, 0).to(torch.float)
+                    binr = (torch.clone(binary_representation[i]))
+                    b = torch.t(binr)
+                    bbt = torch.matmul(b,torch.t(b))
+                    inv = torch.inverse(bbt)
+                    bbtb = torch.matmul(inv,b)
+                    intermediate = torch.matmul(bbtb,f.flatten())
+                    weights[i] = intermediate
+                print('\b' * l,'#',end='',flush=True)
                 pass
             print()
 
             # Forward pass
-            outputs = model(images)
+            outputs = model_q(images)
+            #  print(next(model_q.named_parameters()))
             loss = criterion(outputs, labels)
 
-            # Backward and optimize
-            optimizer_quantized.zero_grad()
-            optimizer_full_precision.zero_grad()
+            #  # Backward and optimize
+            #  optimizer_quantized.zero_grad()
+            #  optimizer_full_precision.zero_grad()
 
             loss.backward()
             for q, f in zip(quantized, full_precision):
